c2badfba3e0ccd084a4de9eab91e75a7c5596607
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2023-09-01 22:55:53 -0400

    Se agregan dependencias para conexión con base de datos

---
+++ NerdClassLibrary/Data/UsuarioData.cs
@@ -0,0 +1,30 @@
+﻿using NerdClassLibrary.DbAccess;
+using NerdClassLibrary.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdClassLibrary.Data
+{
+    public class UsuarioData
+    {
+        private readonly ISqlDataAccess _db;
+public UsuarioData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<Usuario>> GetUsuarios() =>
+            _db.LoadData<Usuario, dynamic>(storedProcedure: "GetUsuarios", new { });
+
+        public async Task<Usuario?> GetUsuario(int id)
+        {
+            var result = await _db.LoadData<Usuario, dynamic>(
+                storedProcedure: "GetUsuario", new { Id = id });
+
+            return result.FirstOrDefault();
+        }
+    }
+}
---
+++ NerdClassLibrary/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+
+namespace NerdClassLibrary.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        /// <summary>
+        /// Conexión a la base de datos para obtener la información utilizando un procedimiento almacenado.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="storedProcedure">Parámetro del procedimiento almacenado que se utilizará al realizar la llamada</param>
+        /// <param name="parameters">parametros en el caso de que se requieraa un ID en específico para un procedimiento almacenado</param>
+        /// <param name="connectionId">String de conexión alojado en el archivo 'appsettings.json'</param>
+        /// <returns>Devuelve un IEnumerable con toda la información solicitada para poder poblar el campos requeridos</returns>
+        public async Task<IEnumerable<T>> LoadData<T, U>(
+            string storedProcedure,
+            U parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(
+            string storedProcedure,
+            T parameters,
+            string connectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
