5d5bf91c012e5ec2150cc3f7aa949e89d0950081
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2023-09-04 22:01:26 -0300

    Avances diminutos del frontend

--- NerdClassLibrary/Modelos/Anime.cs
+++ NerdClassLibrary/Modelos/Anime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NerdClassLibrary.Modelos;
 
@@ -7,14 +8,18 @@ public partial class Anime
 {
     public int Id_Anime { get; set; }
 
+    [Required(ErrorMessage = "Que tonto. No puedes ingresar un Anime sin un nombre.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "Vaya sonso. La sinopsis es necesaria para ingresar un anime.")]
     public string Sinopsis { get; set; } = null!;
 
+    [Required(ErrorMessage = "Si no te la sabes, al menos date el trabajo de inventar algo")]
     public DateTime Lanzamiento { get; set; }
 
     public int Temporadas { get; set; }
 
+    [Required(ErrorMessage = "Googlear no cuesta tanto, hágale")]
     public int CapitulosTotales { get; set; }
 
     public string? Imagen { get; set; }
